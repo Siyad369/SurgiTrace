892fd4393cd13ca9a2e70bd62c428230f6a081f8
najiya-nazin <jiyaaapk@gmail.com>
2026-03-18 22:06:37 +0530

    completed serializer and views

--- surgeries/urls.py
+++ surgeries/urls.py
@@ -1,3 +1,16 @@
+from django.urls import path
+from .views import OperatingRoomListCreateAPIView, OperatingRoomDetailAPIView, SurgeryListCreateAPIView, SurgeryDetailAPIView, UpcomingSurgeriesAPIView, CompletedSurgeriesAPIView, DepartmentSurgeriesAPIView
+
+
 urlpatterns = [
 
+    path('surgeries/', SurgeryListCreateAPIView.as_view(), name='surgery-list-create'),
+    path('surgeries/<int:pk>/', SurgeryDetailAPIView.as_view(), name='surgery-detail'),
+
+    path('surgeries/upcoming/', UpcomingSurgeriesAPIView.as_view(), name='surgery-upcoming'),
+    path('surgeries/completed/', CompletedSurgeriesAPIView.as_view(), name='surgery-completed'),
+    path('surgeries/department/<int:department_id>/', DepartmentSurgeriesAPIView.as_view(), name='surgery-by-department'),
+
+    path('rooms/', OperatingRoomListCreateAPIView.as_view(), name='room-list-create'),
+    path('rooms/<int:pk>/', OperatingRoomDetailAPIView.as_view(), name='room-detail'),
 ]
