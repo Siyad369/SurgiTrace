fbd62f00cec1aa3bd6ab857dc2d45af8c97528cc
Krishnapathirappalli <krishnaunnikrishnan287@gmail.com>
2026-03-19 13:36:27 +0530

    completed views

--- videos/views.py
+++ videos/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from django.http import FileResponse, Http404
 
 from .models import SurgeryVideo
-from .serializers import SurgeryVideoSerializer
+from .serializer import SurgeryVideoSerializer
 
 
 # 📌 GET all videos / GET single / POST upload
