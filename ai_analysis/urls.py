654fd36eabc31f9f2675ad63bf3be598da325331
Siyad369 <aksiyad369@gmail.com>
2026-03-25 22:57:24 +0530

    medgemma integration

---
+++ ai_analysis/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import RunAnalysisAPIView, GetAnalysisAPIView
+
+urlpatterns = [
+    path("analyze/<int:video_id>/", RunAnalysisAPIView.as_view()),
+    path("analysis/<int:video_id>/", GetAnalysisAPIView.as_view()),
+]
