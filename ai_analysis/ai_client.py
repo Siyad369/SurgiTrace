e8ad2b1610c6385a5f8816d58154382842be8dce
Siyad369 <aksiyad369@gmail.com>
2026-03-31 12:40:35 +0530

    ai client error fixing

--- ai_analysis/ai_client.py
+++ ai_analysis/ai_client.py
@@ -1,8 +1,23 @@
 from gradio_client import Client, handle_file
 
 
+# def call_ai_service(video_url):
+#     client = Client("siyad369/sves_space")
+#
+#     result = client.predict(
+#         video=handle_file(video_url),
+#         api_name="/analyze_video"
+#     )
+#
+#     return result
+
 def call_ai_service(video_url):
-    client = Client("siyad369/sves_space")
+    try:
+        # Try repo name first
+        client = Client("siyad369/sves_space")
+    except:
+        # Fallback to URL
+        client = Client("https://siyad369-sves-space.hf.space")
 
     result = client.predict(
         video=handle_file(video_url),
