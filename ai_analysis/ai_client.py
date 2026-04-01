2664c5b30c6c419e196c5cea826768f6dcffad03
Siyad369 <aksiyad369@gmail.com>
2026-04-01 12:08:05 +0530

    r.txt udated

--- ai_analysis/ai_client.py
+++ ai_analysis/ai_client.py
@@ -1,23 +1,8 @@
 from gradio_client import Client, handle_file
 
 
-# def call_ai_service(video_url):
-#     client = Client("siyad369/sves_space")
-#
-#     result = client.predict(
-#         video=handle_file(video_url),
-#         api_name="/analyze_video"
-#     )
-#
-#     return result
-
 def call_ai_service(video_url):
-    try:
-        # Try repo name first
-        client = Client("siyad369/sves_space")
-    except:
-        # Fallback to URL
-        client = Client("https://siyad369-sves-space.hf.space")
+    client = Client("https://siyad369-sves-space.hf.space")
 
     result = client.predict(
         video=handle_file(video_url),
@@ -25,3 +10,18 @@ def call_ai_service(video_url):
     )
 
     return result
+
+# def call_ai_service(video_url):
+#     try:
+#         # Try repo name first
+#         client = Client("siyad369/sves_space")
+#     except:
+#         # Fallback to URL
+#         client = Client("https://siyad369-sves-space.hf.space")
+#
+#     result = client.predict(
+#         video=handle_file(video_url),
+#         api_name="/analyze_video"
+#     )
+#
+#     return result
