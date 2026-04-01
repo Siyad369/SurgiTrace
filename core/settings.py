ce67c1cd3792b9474c143151fe90e932186f784b
Anas <anas@gmail.com>
2026-04-01 09:21:43 +0530

    cors

--- core/settings.py
+++ core/settings.py
@@ -74,7 +74,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://localhost:3000",   
-    "https://surgi-trace.vercel.app/",
+    "https://surgi-trace.vercel.app",
 ]
 
 # CORS_ALLOW_HEADERS = list(default_headers) + [
